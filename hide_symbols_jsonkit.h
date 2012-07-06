cc62a0618c78e2af099e1aa9b05b74452a0122fa
Nathaniel Griswold <nategriswold@gmail.com>
2012-07-06 16:05:14 -0700

    Make symbol hiding generic

--- hide_symbols_jsonkit.h
+++ hide_symbols_jsonkit.h
@@ -1,9 +1,19 @@
-#define JSONDecoder SZJSONDecoder 
-#define JSONKitDeserializing SZJSONKitDeserializing
-#define JSONKitSerializing SZJSONKitSerializing
-#define JKArray SZJKArray
-#define JKDictionaryEnumerator SZJKDictionaryEnumerator
-#define JKDictionary SZJKDictionary
-#define JKMutableDictionary SZJKMutableDictionary
-#define JKSerializer SZJKSerializer
-#define JKMutableArray SZJKMutableArray
+#ifndef JKPREFIX
+#define JKPREFIX JK_HIDDEN_
+#endif
+
+#define JKPASTER(x,y) x ## y
+#define JKEVALUATOR(x,y) JKPASTER(x,y)
+#define JKP(sym) JKEVALUATOR(JKPREFIX, sym)
+
+#define JSONDecoder JKP(JSONDecoder)
+#define JSONKitDeserializing JKP(JSONKitDeserializing)
+#define JSONKitSerializing JKP(JSONKitSerializing)
+#define JKArray JKP(JKArray)
+#define JKDictionaryEnumerator JKP(JKDictionaryEnumerator)
+#define JKDictionary JKP(JKDictionary)
+#define JKMutableDictionary JKP(JKMutableDictionary)
+#define JKSerializer JKP(JKSerializer)
+#define JKMutableArray JKP(JKMutableArray)
+#define jk_collectionClassLoadTimeInitialization JKP(jk_collectionClassLoadTimeInitialization)
+
